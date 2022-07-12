d6ff38facb1ae37330403626acc1c4f8bd78942c
Santhosh_Kumar_Nayak <santhoshkn011@gmail.com>
2022-07-12 23:46:22 +0530

    Added: Handing Exception program added.

--- src/main/java/com/moodanalyser/MoodAnalyserMain.java
+++ src/main/java/com/moodanalyser/MoodAnalyserMain.java
@@ -1,23 +1,33 @@
 /*
 Given “I am in Happy Mood” message in Constructor Should
 Return SAD
+
+Handle Exception if User Provides Invalid Mood
  */
+
 package com.moodanalyser;
 
 public class MoodAnalyserMain {
     public static String message;
-    //constructor
-    public MoodAnalyserMain(String message){
+
+    public MoodAnalyserMain(String message) {
         this.message = message;
         analyseMood();
     }
-    public String analyseMood(){
-        if (message.contains("Sad"))
-            return "SAD";
-        else if (message.contains("Happy"))
-            return "HAPPY";
-        else
-            return "HAPPY";
 
+    public String analyseMood() {
+        try {
+            if (message.contains("Sad"))
+                return "SAD";
+            else if (message.contains("Happy"))
+                return "HAPPY";
+            else
+                return "HAPPY";
+
+        } catch (Exception e) {
+            System.out.println("Null Pointer Exception occurred.");
+        }
+        return null;
     }
 }
+
