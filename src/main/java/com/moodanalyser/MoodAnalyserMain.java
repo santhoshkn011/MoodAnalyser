28fabaf5b330312a23786a1434c56355c46acb74
Santhosh_Kumar_Nayak <santhoshkn011@gmail.com>
2022-07-12 22:46:19 +0530

    Added: Welcome Message Added.

---
+++ src/main/java/com/moodanalyser/MoodAnalyserMain.java
@@ -0,0 +1,7 @@
+package com.moodanalyser;
+
+public class MoodAnalyserMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Mood analyser problem.");
+    }
+}
